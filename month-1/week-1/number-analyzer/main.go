e65b2312c2c3885806102a0284e1e4909fd10915
Algran12 <alulgibran12@gmail.com>
2026-03-01 00:02:37 +0800

    add user input and basic validation
    Add number input handling and output in main function

--- month-1/week-1/number-analyzer/main.go
+++ month-1/week-1/number-analyzer/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Number Analyzer CLI")
+	fmt.Print("Enter a number: ")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	input = strings.TrimSpace(input)
+
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Invalid number input")
+		return
+	}
+
+	fmt.Println("You entered:", number)
 }
